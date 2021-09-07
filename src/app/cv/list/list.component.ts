eea326d81563d5268b9ac8204f439bc179dcf63e
Maxime Bochon <maxime.bochon@gmail.com>
2021-09-07 10:34:55 +0000

    Improve CV app with ngIf, ngFor, odd/even colors

--- src/app/cv/list/list.component.ts
+++ src/app/cv/list/list.component.ts
@@ -18,7 +18,9 @@ export class ListComponent implements OnInit {
     this.cvList = [
       new Cv(1, 'Johann Sebastian', 'BACH', 1685, 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg'),
       new Cv(2, 'Antonio', 'VIVALDI', 1678, 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Antonio_Vivaldi.jpg'),
-      new Cv(3, 'François', 'COUPERIN', 1668, 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Francois_Couperin_2.jpg')
+      new Cv(3, 'François', 'COUPERIN', 1668, 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Francois_Couperin_2.jpg'),
+      new Cv(4, 'Jean-Philippe', 'RAMEAU', 1683, 'https://upload.wikimedia.org/wikipedia/commons/9/98/Attribu%C3%A9_%C3%A0_Joseph_Aved%2C_Portrait_de_Jean-Philippe_Rameau_%28vers_1728%29_-_001.jpg'),
+      new Cv(4, 'Jean-Baptiste', 'LULLY', 1632, 'https://upload.wikimedia.org/wikipedia/commons/8/85/Paul_Mignard_-_Jean-Baptiste_Lully.jpg')
     ]
   }
 
