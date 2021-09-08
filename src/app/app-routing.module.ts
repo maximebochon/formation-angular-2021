5d8487b68fac201c4d8b16a43f77eb009fcfb333
Maxime Bochon <maxime.bochon@gmail.com>
2021-09-08 08:09:55 +0000

    Define routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ColorByTextComponent } from './components/color-by-text/color-by-text.component';
+import { FirstComponent } from './components/first/first.component';
+import { CvComponent } from './cv/cv/cv.component';
+import { TextFormattingComponent } from './directives/text-formatting/text-formatting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'first', component: FirstComponent },
+  { path: 'color', component: ColorByTextComponent },
+  { path: 'text', component: TextFormattingComponent },
+  { path: 'cv', component: CvComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
